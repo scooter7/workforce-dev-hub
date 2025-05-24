bbb1a8284ef0e8c03aea6cb991a1d1c26444fff3
scooter7 <scooter.vineburgh@gmail.com>
2025-05-24 16:34:45 -0500

    quiz page fix2

--- src/components/quizzes/QuizPlayer.tsx
+++ src/components/quizzes/QuizPlayer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, useRef } from 'react';
-import { QuizData, QuizQuestion, QuestionOption } from '@/types/quiz';
+import { QuizData, QuizQuestion } from '@/types/quiz';
 import Button from '@/components/ui/Button';
 import { useRouter } from 'next/navigation';
 import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/solid'; // For correctness indication
